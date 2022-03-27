74ace71c22dfb8a97749943a989d31a420461ed3
samson-favour <favoursamson347@gmail.com>
2022-03-26 17:46:25 -0700

    project added

---
+++ app.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+const SwaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+const express = require("express");
+const app = express();
+
+// connectDB
+
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 16 * 1000,
+    max: 100,
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
+
+app.get('/', (req, res)=> {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+// routes
+app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerDocument))
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
